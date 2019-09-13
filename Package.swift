f7578539b1206eb6d2b0c25cb7e28a5de84511b8
Pavel Sharanda <edvaef@gmail.com>
2019-09-13 23:38:12 +0300

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let package = Package(
-    name: "Easing"
-)
+let package = Package(name: "Easing",
+                      platforms: [.macOS(.v10_10),
+                                  .iOS(.v8),
+                                  .tvOS(.v9),
+                                  .watchOS(.v2)],
+                      products: [.library(name: "Easing",
+                                          targets: ["Easing"])],
+                      targets: [.target(name: "Easing",
+                                        path: "Sources"),
+                                .testTarget(
+                                    name: "EasingTests",
+                                    dependencies: ["Easing"]),],
+                      swiftLanguageVersions: [.v5])
